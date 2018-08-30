237fe44dc5e442adc9b7723e5ef8813d3c90de8a
Tatsuro Miyazaki <tatsumiya05@gmail.com>
2018-08-30 16:54:06 +0900

    update a file

--- arihon/template.cpp
+++ arihon/template.cpp
@@ -23,6 +23,8 @@ using namespace std;
 #define repm(i, a, b) for(int i = (int)a; i > (int)b; i--)
 #define bit(a) bitset<8>(a)
 #define des_priority_queue priority_queue<int, vector<int>, greater<int> >
+#define all(v) (v).begin(), (v).end()
+#define initArray(a, array_size, n) fill(a, a + array_size, n)
 typedef long long ll;
 typedef pair<int, int> P;
 const ll INF = 1e18;
