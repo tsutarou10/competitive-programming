385be4b215c105679faf8c30dd27ce843044e2ca
Tatsuro Miyazaki <tatsumiya05@gmail.com>
2018-09-06 04:53:07 +0900

    update a file

--- arihon/template.cpp
+++ arihon/template.cpp
@@ -16,17 +16,18 @@ using namespace std;
 #define vsort(v) sort(v.begin(), v.end())
 // descending order
 #define vsort_r(v) sort(v.begin(), v.end(), greater<int>())
-#define vunique(v) unique(v.begin(), v.end())
+#define vunique(v) v.erase(unique(v.begin(), v.end()), v.end())
 #define mp make_pair
 #define ts(x) to_string(x)
 #define rep(i, a, b) for(int i = (int)a; i < (int)b; i++)
 #define repm(i, a, b) for(int i = (int)a; i > (int)b; i--)
 #define bit(a) bitset<8>(a)
 #define des_priority_queue priority_queue<int, vector<int>, greater<int> >
-#define all(v) (v).begin(), (v).end()
 typedef long long ll;
 typedef pair<int, int> P;
 const ll INF = 1e18;
+
+#define MAX_V 1000000
 
 int main(){
 	cin.tie(0);
