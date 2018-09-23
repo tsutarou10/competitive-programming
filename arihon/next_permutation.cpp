0c4cbb8553e2f9d67c3af6babe395ed9f5ab0184
Tatsuro Miyazaki <tatsumiya05@gmail.com>
2018-09-23 23:20:01 +0900

    update a file

--- arihon/next_permutation.cpp
+++ arihon/next_permutation.cpp
@@ -16,45 +16,28 @@ using namespace std;
 #define vsort(v) sort(v.begin(), v.end())
 // descending order
 #define vsort_r(v) sort(v.begin(), v.end(), greater<int>())
-#define vunique(v) unique(v.begin(), v.end())
+#define vunique(v) v.erase(unique(v.begin(), v.end()), v.end())
 #define mp make_pair
 #define ts(x) to_string(x)
 #define rep(i, a, b) for(int i = (int)a; i < (int)b; i++)
 #define repm(i, a, b) for(int i = (int)a; i > (int)b; i--)
 #define bit(a) bitset<8>(a)
 #define des_priority_queue priority_queue<int, vector<int>, greater<int> >
-#define all(v) (v).begin(), (v).end()
-#define initArray(a, array_size, n) fill(a, a + array_size, n)
 typedef long long ll;
 typedef pair<int, int> P;
 const ll INF = 1e18;
 
-const int MAX = 510000;
-const int mod = 1e9 + 7;
-
-ll fac[MAX], finv[MAX], inv[MAX];
-
-void comInit() {
-	fac[0] = fac[1] = 1;
-	finv[0] = finv[1] = 1;
-	inv[1] = 1;
-	rep(i, 2, MAX) {
-		fac[i] = fac[i - 1] * i % mod;
-		inv[i] = mod - inv[mod % i] * (mod / i) % mod;
-		finv[i] = finv[i - 1] * inv[i] % mod;
-	}
-}
-
-ll comb(ll n, ll k) {
-	if(n < k) return 0;
-	if(n < 0 || k < 0) return 0;
-	return fac[n] * (finv[k] * finv[n - k] % mod) % mod;
-}
+#define MAX_V 1000000
 
 int main(){
 	cin.tie(0);
 	ios::sync_with_stdio(false);
-	comInit();
+	const int N = 3;
+	vector<int> v(N);
+	rep(i, 0, N) v[i] = i;
+	do {
+		rep(i, 0, N) cout << v[i] << " "; cout << endl;
+	} while(next_permutation(v.begin(), v.end()));
 
-	cout << comb(100000, 50000) << endl;
+
 }
